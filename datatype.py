6de7b88a0404287572aeaafd4c05f957552f2206
Shivanshu Tiwari <shuvanshutiwarishivanshu@gmail.com>
2025-01-18 18:56:49 +0530

    Add files via upload
    Data types are used to define the type of a variable. Every variable in Python is an instance of some data type. Python has a variety of built-in data types, categorized into basic types, sequence types, set types.

---
+++ datatype.py
@@ -0,0 +1,17 @@
+# datatype example
+name="shivanshu"  # string
+age = 34          # integer
+marks=64.76        # float
+x=2+3j             #complex
+result = True       #boolean
+a=None             #none
+print(x)
+print(name)
+print(age)
+print(result)
+print(type(x))
+print(type(age))
+print(type(name))
+print(type(a))
+print(type(result))
+print(type(marks))
