f2a2a684678c3a7d1b48fe4373d5ae5b7f4c65d5
Shivanshu Tiwari <shuvanshutiwarishivanshu@gmail.com>
2025-01-18 18:55:12 +0530

    Add files via upload

---
+++ expression.py
@@ -0,0 +1,18 @@
+#expression handling
+a,b=2,3
+txt="@"
+A="2"
+print(a*txt*b)
+print((A+txt)*b)
+
+#result always flot if
+x,y=2,77
+z=9
+print(x+y*z)
+print(b//a)
+print(a/b)
+v=-9
+print(v//z)
+
+
+
