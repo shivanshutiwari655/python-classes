95110fe2137c649dd6c471eb5b5473cf98357a98
Shivanshu Tiwari <shuvanshutiwarishivanshu@gmail.com>
2025-01-18 18:53:52 +0530

    Add files via upload
    Our first program in python programing language. 
    Where print hello word and take a input from the user and calculate the value add and subtract the value.

---
+++ first_program.py
@@ -0,0 +1,29 @@
+# hello world
+
+print("hello world")
+print("hii shivanshu wellcome in coding computer")
+
+# input keyword is used to take a input from the user
+a=int(input("enter the first number :"))  
+b=int(input("Enter the secomd number:"))
+sub=a-b
+sum=a+b
+print(sum)
+print(sub)
+
+# add,sub,mul,dev,rem
+a=int(input("enter the number:"))
+b=int(input("enter the second number:"))
+print(a - b)
+print(a + b)
+print(a / b)
+print(a * b)
+print(a % b)
+
+print("hello world\n","hii")
+name=("shi")
+print("shivanshu")
+sum=4+6
+print(type(sum))
+name2=name
+print(type(name2))
