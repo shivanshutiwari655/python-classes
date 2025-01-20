538ccc7abf4f3d75e55a3ca375f108d27bd3f0aa
Shivanshu Tiwari <shuvanshutiwarishivanshu@gmail.com>
2025-01-20 13:11:03 +0530

    Add files via upload

---
+++ continue_break_pass.py
@@ -0,0 +1,13 @@
+# break,continue and pass loop contol stetment
+for x in range(1,20):
+    if(x==18):
+        break
+    # break keyword is used break the loop
+    if(x==4):
+        continue
+    # continue keyword is used to skip the loop
+    if(x==6):
+        pass
+    # pass keyword is used to do nothing 
+    print(x)
+    
